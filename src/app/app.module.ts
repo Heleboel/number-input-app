6b964b8bb372e22bfe235c7402875c1fcac29ab4
Frits Boel <fgboel@istruction.nl>
2020-04-23 21:10:06 +0200

    Created skeleton for number-input demo app.

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NumberInputComponent } from './components/number-input/number-input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NumberInputComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
