c69f4611d6d256cdc0b971bdb8866a5699c94d37
Frits Boel <fgboel@istruction.nl>
2022-02-27 08:09:05 +0100

    Updated packages
    Updated Angular to version 13.1.1. All other packages updated to their
    newest versions. Fixed some deprecated things and things that caused
    errors.

--- karma.conf.js
+++ karma.conf.js
@@ -4,29 +4,49 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
+    frameworks: [
+      'jasmine',
+      '@angular-devkit/build-angular'
+    ],
+
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/number-input-app'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: [
+        'html',
+        'lcovonly',
+        'text-summary'
+      ],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+
+    reporters: [
+      'progress',
+      'kjhtml'
+    ],
+
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
     singleRun: false,
     restartOnFileChange: true
+  });
+
+  process.on('infrastructure_error', (error) => {
+    console.error('infrastructure_error', error);
   });
 };
